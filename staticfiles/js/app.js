1dde0185d0b0f2010c9a7944b16a9ff89addef2a
Vishal Dhakal <vishaldhakal@Vishals-Air.domain.name>
2020-10-02 16:54:20 +0545

    ASDC

--- staticfiles/js/app.js
+++ staticfiles/js/app.js
@@ -1,4 +1,3 @@
-const toggleBtn = document.querySelector('.togglebtn');
 const menuIcon = document.querySelector('.menu');
 const images = document.querySelector('#myimages')
 const anim = document.querySelectorAll('.anim');
@@ -17,32 +16,11 @@ anim.forEach(element => {
     observer.observe(element);
 });
 
-toggleBtn.addEventListener('click',toggleClick)
-function toggleClick(e){
-    if(menuIcon.classList.contains('abc')){
-        menuIcon.src = "../images/cross.svg";
-        menuIcon.classList.remove('abc');
-    }else{
-        menuIcon.src = "../images/menu.svg";
-        menuIcon.classList.add('abc');
-    }
-}
+
 /* var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
     keyboard: false
 }) */
 
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.glider'), {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        draggable: true,
-        dots: '.dots',
-        arrows: {
-            prev: '.glider-prev',
-            next: '.glider-next'
-        }
-      });      
-})
 /* if (window.matchMedia('(display-mode: standalone)').matches) {
         console.log('Running in standalone');
       }else{
